f092af060303ffa68f124f7c5a1f89acd6b797db
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 09:54:32 +0530

    first commit

---
+++ Creational/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import lk.ijse.desingPatten.Singlton;
+
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 5/9/2024
+ */
+public class Main  {
+
+    public static void main(String[] args) {
+        Singlton singlton = Singlton.getInstance();
+        System.out.println(singlton);
+        }
+}
---
+++ Creational/src/main/java/lk/ijse/desingPatten/Singlton.java
@@ -0,0 +1,30 @@
+package lk.ijse.desingPatten;
+
+import java.io.Serializable;
+
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 5/9/2024
+ */
+public class Singlton implements Serializable {
+
+    private Singlton(){}
+
+    private static Singlton singlton = new Singlton();
+
+    public static Singlton getInstance(){
+//        if (singlton == null){
+//            synchronized(Singlton.class){
+//                if (singlton == null){
+//                    singlton = new Singlton();//instance will be created at request time
+//                }
+//            }
+//        }
+        return singlton;
+    }
+
+
+
+
+}
