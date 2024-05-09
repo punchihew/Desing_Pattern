0d756a0d02597f86a1b47922cca5262a09b6e6cf
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 10:36:48 +0530

    desing patten

--- Structure/Facade/src/main/java/Rectangle.java
+++ Structure/Facade/src/main/java/Rectangle.java
@@ -6,6 +6,6 @@
 public class Rectangle implements Shape{
     @Override
     public void draw() {
-        System.out.println("RECTANGLE call method");
+        System.out.println("RECTANGLE : Call Rectangle Method");
     }
 }
