a1ecdc7c488ed86226d05d47c78176a4dfa6dacd
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 11:19:06 +0530

    desing patten

--- Structure/Decorator/src/main/java/DecoratorDemo.java
+++ Structure/Decorator/src/main/java/DecoratorDemo.java
@@ -6,5 +6,14 @@
 public class DecoratorDemo {
     public static void main(String[] args) {
 
+        Shape circle = new Circle();
+
+        Shape redCircle = new RedShapeDecorator(new Circle());
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+
+        System.out.println("Circle : Normal Border");
+        circle.draw();
+        System.out.println("\n Circle : Add Red Border");
+        redCircle.draw();
     }
 }
