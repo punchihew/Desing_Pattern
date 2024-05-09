09ac2788de4f94f75566d2157b4696254a5252dc
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 10:56:13 +0530

    desing patten

--- Structure/Decorator/src/main/java/DecoratorDemo.java
+++ Structure/Decorator/src/main/java/DecoratorDemo.java
@@ -1,6 +1,7 @@
-package PACKAGE_NAME;/**
-@version: v0.0.1
-@author: Navishka
-@date: 5/9/2024
-*/public class DecoratorDemo {
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 5/9/2024
+ */
+public class DecoratorDemo {
 }
