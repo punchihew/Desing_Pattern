25de27d7916fc141baab9e4232974cd809d4bb63
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 11:42:21 +0530

    desing patten

--- Behivior/Strategy/src/main/java/OperationSubcriber.java
+++ Behivior/Strategy/src/main/java/OperationSubcriber.java
@@ -3,5 +3,9 @@
  * @author: Navishka
  * @date: 5/9/2024
  */
-public class OperationSubcriber {
+public class OperationSubcriber implements Strategy{
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1-num2;
+    }
 }
