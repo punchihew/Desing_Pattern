c839603fa708384656ba705151033e3fbb632851
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 11:52:38 +0530

    desing patten

--- Behivior/Strategy/src/main/java/StrategyDemo.java
+++ Behivior/Strategy/src/main/java/StrategyDemo.java
@@ -4,4 +4,12 @@
  * @date: 5/9/2024
  */
 public class StrategyDemo {
+
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println(context.executeStrategy(10,30));
+
+        context = new Context(new OperationSubcriber());
+        System.out.println(context.executeStrategy(10,30));
+    }
 }
