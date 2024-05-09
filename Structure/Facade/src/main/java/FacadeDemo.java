15966cd947242d81975fc5e7129a546197343ac7
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 10:39:36 +0530

    desing patten

--- Structure/Facade/src/main/java/FacadeDemo.java
+++ Structure/Facade/src/main/java/FacadeDemo.java
@@ -5,7 +5,10 @@
  */
 public class FacadeDemo {
     public static void main(String[] args) {
+
           Shape shape = new Circle();
           shape.draw();
+          Shape shape1 = new Circle();
+          shape1.draw();
     }
 }
---
+++ Structure/Facade/src/main/java/ShapeMaker.java
@@ -0,0 +1,11 @@
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 5/9/2024
+ */
+public class ShapeMaker {
+
+    private Shape circle;
+    private Shape rectangle;
+
+}
