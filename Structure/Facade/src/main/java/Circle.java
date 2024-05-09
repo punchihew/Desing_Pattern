aff31ea3039dec22abcdedd27e6138cae63b2212
punchihewa <navishkapunchihewa66@gmail.com>
2024-05-09 10:34:11 +0530

    desing patten

---
+++ Structure/Facade/src/main/java/Circle.java
@@ -0,0 +1,11 @@
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 5/9/2024
+ */
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("CIRCLE");
+    }
+}
--- Structure/Facade/src/main/java/Shape.java
+++ Structure/Facade/src/main/java/Shape.java
@@ -1,6 +1,9 @@
-package PACKAGE_NAME;/**
-@version: v0.0.1
-@author: Navishka
-@date: 5/9/2024
-*/public interface Shape {
+/**
+ * @version: v0.0.1
+ * @author: Navishka
+ * @date: 5/9/2024
+ */
+public interface Shape {
+
+    void draw();
 }
